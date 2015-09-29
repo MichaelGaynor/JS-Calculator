30e9663759e7675a7cc49e65fe335997c4bae9f6
Michael Gaynor <jmichaelgaynor@gmail.com>
2015-09-29 15:35:25 -0400

    Buttons added and basic css

---
+++ Calculator.js
@@ -0,0 +1,23 @@
+//Buttons
+var btnOne = document.querySelector('#one');
+var btnTwo = document.querySelector('#two');
+var btnThree = document.querySelector('#three');
+var btnFour = document.querySelector('#four');
+var btnFive = document.querySelector('#five');
+var btnSix = document.querySelector('#six');
+var btnSeven = document.querySelector('#seven');
+var btnEight = document.querySelector('#eight');
+var btnNine = document.querySelector('#nine');
+var btnZero = document.querySelector('#zero');
+var btnClear = document.querySelector('#clear');
+var btnEql = document.querySelector('#equals');
+var btnPnt = document.querySelector('#point');
+var btnAdd = document.querySelector('#add');
+var btnSub = document.querySelector('#subtract');
+var btnTimes = document.querySelector('#times');
+var btnDivide = document.querySelector('#divide');
+
+//Button click
+buttonOne.addEventListener("click", function(){
+
+});
