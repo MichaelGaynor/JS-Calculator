c8f2b6589ddca77d83ac1d65c116d0cbc318712c
Michael Gaynor <jmichaelgaynor@gmail.com>
2015-09-29 14:53:33 -0400

    GOT IT

---
+++ main.js
@@ -0,0 +1,23 @@
+//Grabbing Elements
+var numOne = document.querySelector('#num_one');
+var numTwo = document.querySelector('#num_two');
+var calcBtn = document.querySelector('#calculateBtn')
+var answer = document.querySelector('#answer');
+
+//Declaring Variable
+var solution;
+
+
+//Find Answer
+var mathIt = function(){
+return (Number(numOne.value) + Number(numTwo.value)) 
+};
+
+//Display Answer
+var showMe = function(){
+  solution = mathIt();
+  answer.textContent = solution;
+};
+
+//Button Click
+calcBtn.addEventListener('click', showMe)
